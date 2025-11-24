e2c9e4fdbd855f7c2ced987b0e5ee0c1f9e0b6d1
clara-pinto <clara.pinto@edu.esiee.fr>
2025-11-24 15:35:45 +0000

    Add history

--- player.py
+++ player.py
@@ -31,11 +31,13 @@ class Player():
     def __init__(self, name):
         self.name = name
         self.current_room = None
+        self.history = []
     
     # Define the move method.
     def move(self, direction):
         # Get the next room from the exits dictionary of the current room.
         next_room = self.current_room.exits[direction]
+        self.history.append(self.current_room)
 
         # If the next room is None, print an error message and return False.
         if next_room is None:
@@ -46,5 +48,15 @@ class Player():
         self.current_room = next_room
         print(self.current_room.get_long_description())
         return True
+    
+    def get_history():
+        print("Vous avez déjà visité les lieux suivants :\n")
+        for elt in self.history:
+            print("\t- ",elt,"\n")
+
+
+           
+
+
 
     
